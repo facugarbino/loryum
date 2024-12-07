346363d9d4e6f16231a261ac1050e3cf3f24a38b
Facundo Garbino <facugarbino2@gmail.com>
2024-12-07 11:38:35 -0300

    Allow upto 5mb of body in serverActions

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "5mb", //To allow sending up to 4 images, 1MB each
+    },
+  },
+};
 
 module.exports = nextConfig;
